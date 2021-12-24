412e2f95c69e39d2ed4d6e0c9326801048decac0
plowsof <77655812+plowsof@users.noreply.github.com>
2021-12-24 00:59:07 +0000

    Add files via upload

--- static_html_loop.py
+++ static_html_loop.py
@@ -5,6 +5,13 @@ import random
 from filelock import FileLock
 import os
 import json
+import pprint
+#todo handle when goal is overfunded
+
+prices = {}
+prices["monero"] = 0
+prices["bitcoin-cash"] = 0
+prices["bitcoin"] = 0
 
 def main(config):
     www_root = config["wishlist"]["www_root"]
@@ -19,30 +26,84 @@ def main(config):
     with open(json_file,'r') as f:
         wishlist = json.load(f)
     html = ""
+    set_global_prices()
     for wish in wishlist["wishlist"]:
-        total = get_total(wish)
+        our_data = get_total(wish)
+        total = round(our_data["total_usd"],2)
+        sortme = our_data["values"]
+
+        sortme = dict(sorted(sortme.items(), key=lambda item: item[1]))
+        pprint.pprint(sortme)
         xmr_history = get_history(wish["xmr_history"])
         bch_history = get_history(wish["bch_history"])
         btc_history = get_history(wish["btc_history"])
         wish["percent"] = (total / wish["goal_usd"]) * 100
-        htmlSegment =f"""  <div class ="wish">
-                    <li>
-                        <span class="wish_title">{wish["title"]}</span></br>
-                        <p class="description">{wish["description"]}</p>
-                        <i class="fundgoal">Raised ${total} of ${wish["goal_usd"]} <progress id="file" max="100" value="{wish["percent"]}">{wish["percent"]}%</progress> Contributors: {wish["contributors"]}</i>
-                        <label for="file"></label><br/>
-                        <img class="ticker" src="./logos/monero-xmr-logo.png" alt="XMR">
-                        <p class="xmr_address" id="{wish["id"]}">{wish["xmr_address"]}</br></p>
-                        <p>[<a href="{wish["qr_img_url_xmr"]}" data-lightbox="{wish["id"]}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{xmr_history}</span></span></p>
-                        <img class="ticker" src="./logos/bitcoin-cash-bch-logo.png" alt="BCH">
-                        <p class="bch_address" id="{wish["id"]}">{wish["bch_address"]}</br></p>
-                        <p>[<a href="{wish["qr_img_url_bch"]}" data-lightbox="{wish["id"]}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{bch_history}</span></span></p>
-                        <img class="ticker" src="./logos/BTC_Logo.png" alt="BTC">
-                        <p class="btc_address" id="{wish["id"]}">{wish["btc_address"]}</br></p> 
-                        <p>[<a href="{wish["qr_img_url_btc"]}" data-lightbox="{wish["id"]}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{btc_history}</span></span>
-                    </li>
-                </div>""";
-        html += htmlSegment;
+        colours = {
+          "monero": "#f26822",
+          "bitcoin-cash": "#0ac18e",
+          "bitcoin": "#f7931a"
+        }
+        sort = list(sortme.keys())
+        one = colours[sort[0]]
+        prc0 = sortme[sort[0]]
+        one += f" {prc0}%"
+
+
+        two = colours[sort[1]]
+        prc1 = sortme[sort[1]]
+        prc1 += prc0
+        two += f" {prc0}% {prc1}%"
+
+        three = colours[sort[2]]
+        prc2 = sortme[sort[2]]
+        prc2 += prc1
+        three += f" {prc1}% {prc2}%"
+        end = f"{prc2}%"
+
+        htmlSegment =f"""  
+            <style>
+             .progress_{wish['id']} {{
+             border-radius: 25px;
+             height:10px;
+             width:100%;
+             border:2px solid #000;
+             text-align:center;
+             color:#fff;
+             font-size:20px;
+             background: linear-gradient(to right,
+                 {one}, {two}, {three}, transparent {end});
+            }}
+            </style>
+            <div class ="wish">
+            <span class="wish_title"><h3>{wish['title']}</h3></span></br>
+            <div class="progress_{wish['id']}"></div>
+                <p class="fundgoal">Raised ${total} of ${wish['goal_usd']} Contributors: {wish['contributors']}</p>
+                <p class="description">{wish['description']}</p>
+                <br/>
+                <div class="tabs">
+                  <input type="checkbox" name="tabs" id="tabone_{wish['id']}">
+                  <label class="cointab" for="tabone_{wish["id"]}"><img class="ticker" src="./logos/monero-xmr-logo.png" alt="XMR"></label>
+                  <div class="tab">
+                  <p class="xmr_address" id="{wish['id']}">{wish['xmr_address']}</br></p>
+                  <p>[<a href="{wish['qr_img_url_xmr']}" data-lightbox="{wish['id']}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{xmr_history}</span></span></p>
+                  </div>
+                  
+                  <input type="checkbox" name="tabs" id="tabtwo_{wish['id']}">
+                  <label class="cointab" for="tabtwo_{wish["id"]}"><img class="ticker" src="./logos/bitcoin-cash-bch-logo.png" alt="BCH"></label>
+                  <div class="tab">
+                  <p class="bch_address" id="{wish['id']}">{wish['bch_address']}</br></p>
+                  <p>[<a href="{wish['qr_img_url_bch']}" data-lightbox="{wish['id']}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{bch_history}</span></span></p>
+                  </div>
+                  
+                  <input type="checkbox" name="tabs" id="tabthree_{wish['id']}">
+                  <label class="cointab" for="tabthree_{wish["id"]}"><img class="ticker" src="./logos/BTC_Logo.png" alt="BTC"></label>
+                  <div class="tab">
+                  <p class="btc_address" id="{wish['id']}">{wish['btc_address']}</br></p> 
+                  <p>[<a href="{wish['qr_img_url_btc']}" data-lightbox="{wish['id']}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">{btc_history}</span></span>
+                  </div>
+                </div>
+        </div> <hr>"""
+        html += htmlSegment
     replacement = ""
     with open(funding_template, 'r') as f:
         for line in f:
@@ -66,22 +127,50 @@ def get_history(data):
             i = len(data)
     return history
 
+def set_global_prices():
+    global prices
+    ran_int = random.randint(1, 10000)
+    for x in prices:
+        url_get = f"https://api.coingecko.com/api/v3/simple/price?ids={x}&vs_currencies=usd&uid=" + str(ran_int)
+        resp = requests.get(url=url_get)
+        data = resp.json()
+        prices[x] = data[x]["usd"]
+    pprint.pprint(prices)
+
 def get_total(wish):
+    prices
     ticker_var = {
         "monero": "xmr_total",
         "bitcoin": "btc_total",
         "bitcoin-cash": "bch_total"
     }
+
+    returnme = {}
+    returnme["values"] = {
+    "monero": 0,
+    "bitcoin-cash": 0,
+    "bitcoin": 0
+    }
+
     total_usd = 0
+    total_percent = 0
     for x in ticker_var:
-        ran_int = random.randint(1, 10000)
-        url_get = f"https://api.coingecko.com/api/v3/simple/price?ids={x}&vs_currencies=usd&uid=" + str(ran_int)
-        resp = requests.get(url=url_get)
-        data = resp.json()
         coin = ticker_var[x]
-        usd = data[x]["usd"]
-        total_usd += (usd * wish[coin])
-    return round(total_usd,2)
+        usd = prices[x]
+        this_coin = usd * wish[coin]
+        percent = (this_coin / wish["goal_usd"]) * 100
+        total_percent += percent
+        print(f"this_coin = {this_coin} \n wish goal = {wish['goal_usd']}\n percent = {percent}")
+        returnme["values"][x] = percent
+        total_usd += (this_coin)
+
+    if total_percent >= 100:
+        #fully funded 
+        for x in returnme["values"]:
+            returnme["values"][x] = (returnme["values"][x] / total_percent) * 100
+
+    returnme["total_usd"] = round(total_usd,2)
+    return returnme
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
