e4366801bc0e8888f6e5169cd2dc61b32e7bfc2a
plowsof <77655812+plowsof@users.noreply.github.com>
2021-12-28 14:34:15 +0000

    indentation error

--- notify_bch_btc.py
+++ notify_bch_btc.py
@@ -64,57 +64,57 @@ class S(BaseHTTPRequestHandler):
             daemon_path = btc_path
             ticker = "btc"
 
-            ticker_address = f"{ticker}_address"
-            ticker_total = f"{ticker}_total"
-            ticker_history = f"{ticker}_history"
-            ticker_con = f"{ticker}_confirmed"
-            ticker_unc = f"{ticker}_unconfirmed"
-            status = thejson["status"]
-            print(f"daemon_path: {daemon_path}")
-            stream = os.popen(f"{daemon_path} getaddressbalance {address} --testnet")
-            json_output = stream.read().replace("\n","")
-            print(repr(json_output))
-            output = json.loads(json_output)
-            address_con = output["confirmed"]
-            address_unc = output["unconfirmed"]
-            print(f"conf {address_con}, unc {address_unc}")
+        ticker_address = f"{ticker}_address"
+        ticker_total = f"{ticker}_total"
+        ticker_history = f"{ticker}_history"
+        ticker_con = f"{ticker}_confirmed"
+        ticker_unc = f"{ticker}_unconfirmed"
+        status = thejson["status"]
+        print(f"daemon_path: {daemon_path}")
+        stream = os.popen(f"{daemon_path} getaddressbalance {address} --testnet")
+        json_output = stream.read().replace("\n","")
+        print(repr(json_output))
+        output = json.loads(json_output)
+        address_con = output["confirmed"]
+        address_unc = output["unconfirmed"]
+        print(f"conf {address_con}, unc {address_unc}")
 
-            now_balance = float(address_unc) + float(address_con)
+        now_balance = float(address_unc) + float(address_con)
 
-            data_fname = os.path.join(www_root,"data","wishlist-data.json")
-            with open(data_fname, "r") as f:
-                data_wishlist = json.load(f)
-            for i in range(len(data_wishlist["wishlist"])):
-                wishlist_address = data_wishlist["wishlist"][i][ticker_address]
-                if ":" in wishlist_address:
-                    wishlist_address = wishlist_address.split(":")[1]
-                print(f"is {wishlist_address} == {address}:")
-                if  wishlist_address == address:
-                    print("yes")
-                    old_balance = float(data_wishlist["wishlist"][i][ticker_unc]) + float(data_wishlist["wishlist"][i][ticker_con])
-                    if old_balance < now_balance:
-                        #print(f"old balance: {old_balance} new balance : {now_balance}")
-                        amount = float(now_balance) - float(old_balance)
-                        data_wishlist["wishlist"][i][ticker_con] = float(address_con)
-                        data_wishlist["wishlist"][i][ticker_unc] = float(address_unc)
-                        data_wishlist["wishlist"][i][ticker_total] = float(now_balance)
-                        tx_info = {
-                                    "amount":amount, 
-                                    "date_time":str(datetime.now())
-                                  }
-                        data_wishlist["wishlist"][i][ticker_history].append(tx_info)
-                        data_wishlist["wishlist"][i]["contributors"] += 1
-                        modified = str(datetime.now())
-                        data_wishlist["metadata"]["modified"] = modified
-                        lock = FileLock(f"{data_fname}.lock")
-                        with lock:
-                            with open(data_fname, "w+") as f:
-                                json.dump(data_wishlist, f, indent=2) 
-                    
-                    #end the loop early
-                    break
-                else:
-                    print("not equal")
+        data_fname = os.path.join(www_root,"data","wishlist-data.json")
+        with open(data_fname, "r") as f:
+            data_wishlist = json.load(f)
+        for i in range(len(data_wishlist["wishlist"])):
+            wishlist_address = data_wishlist["wishlist"][i][ticker_address]
+            if ":" in wishlist_address:
+                wishlist_address = wishlist_address.split(":")[1]
+            print(f"is {wishlist_address} == {address}:")
+            if  wishlist_address == address:
+                print("yes")
+                old_balance = float(data_wishlist["wishlist"][i][ticker_unc]) + float(data_wishlist["wishlist"][i][ticker_con])
+                if old_balance < now_balance:
+                    #print(f"old balance: {old_balance} new balance : {now_balance}")
+                    amount = float(now_balance) - float(old_balance)
+                    data_wishlist["wishlist"][i][ticker_con] = float(address_con)
+                    data_wishlist["wishlist"][i][ticker_unc] = float(address_unc)
+                    data_wishlist["wishlist"][i][ticker_total] = float(now_balance)
+                    tx_info = {
+                                "amount":amount, 
+                                "date_time":str(datetime.now())
+                              }
+                    data_wishlist["wishlist"][i][ticker_history].append(tx_info)
+                    data_wishlist["wishlist"][i]["contributors"] += 1
+                    modified = str(datetime.now())
+                    data_wishlist["metadata"]["modified"] = modified
+                    lock = FileLock(f"{data_fname}.lock")
+                    with lock:
+                        with open(data_fname, "w+") as f:
+                            json.dump(data_wishlist, f, indent=2) 
+                
+                #end the loop early
+                break
+            else:
+                print("not equal")
 
 def run(server_class=HTTPServer, handler_class=S, port=8080, config=[]):
     #load some json stuff
