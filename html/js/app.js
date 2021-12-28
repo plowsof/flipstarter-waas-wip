0bdf96a567cb38da6b5cea69c838f109a9562eee
plowsof <77655812+plowsof@users.noreply.github.com>
2021-12-28 02:53:33 +0000

    html files

---
+++ html/js/app.js
@@ -0,0 +1,470 @@
+let modified = "0"
+
+function debugWishlist() {
+    wishlist = {
+    "wishlist": [
+        {
+            "goal_usd": 14000.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Plant flowers all over the city. Hand out flyers and spread the word of the humble yet important bee.",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:57.501181",
+            "modified_date": "2021-12-22 00:13:57.501238",
+            "author_name": "",
+            "author_email": "",
+            "id": "75yaxvaiQMWe",
+            "qr_img_url_xmr": "qrs/75yaxvaiQMWe.png",
+            "qr_img_url_btc": "qrs/tb1qn8z6nky4.png",
+            "qr_img_url_bch": "qrs/qz8lsyln79nr.png",
+            "title": "Save the bees",
+            "btc_address": "tb1qn8z6nky4lepc7qssl4mav06gjmxlpflh6tw05n",
+            "bch_address": "bchtest:qz8lsyln79nr9f3xk8y4xzwymu3f2f3g4urgadh3gk",
+            "xmr_address": "75yaxvaiQMWe8ztnLgABmcVskJ8Ra3awTUfFbi7pEvUu5uGNTRVtTdMBSnxriDLHL7NTQ2JWvHof29NknAKvDCFpV7g55Q8",
+            "btc_total": 2,
+            "xmr_total": 10,
+            "bch_total": 20,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 141400.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Make sonic great again! We're going to build hedgehog hotels and distribute them around the city and also inform people about the importance of the hedgehog!",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:57.738618",
+            "modified_date": "2021-12-22 00:13:57.738635",
+            "author_name": "",
+            "author_email": "",
+            "id": "73aMdpQpUhYN",
+            "qr_img_url_xmr": "qrs/73aMdpQpUhYN.png",
+            "qr_img_url_btc": "qrs/tb1qlvljy9un.png",
+            "qr_img_url_bch": "qrs/qp0pvrfy2gay.png",
+            "title": "Save the hedgehogs",
+            "btc_address": "tb1qlvljy9unelqa6g3svn6zk0w9q37ucvf78vtyps",
+            "bch_address": "bchtest:qp0pvrfy2gayy2yezr3l32xc4ukg23yxyg3h3mj7xw",
+            "xmr_address": "73aMdpQpUhYNxMQARwm8Th1gzX1fZo4Yr4wJKG8MVwcVZPkMLD4swgWeJMe72Fa1jCRjY9Y8FByMMQ8kDgwWCgmnEuFuGgG",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 280.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Help me keep the lights on! ",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:57.847368",
+            "modified_date": "2021-12-22 00:13:57.847385",
+            "author_name": "",
+            "author_email": "",
+            "id": "7Aj2ozF5HRvd",
+            "qr_img_url_xmr": "qrs/7Aj2ozF5HRvd.png",
+            "qr_img_url_btc": "qrs/tb1qa8efc4ap.png",
+            "qr_img_url_bch": "qrs/qqfunutvtyjs.png",
+            "title": "Server costs",
+            "btc_address": "tb1qa8efc4apn8vt2f8cknpmj809q4n8vpj6e3s677",
+            "bch_address": "bchtest:qqfunutvtyjst7gu6cze847u24kyrpseguf7ylad5u",
+            "xmr_address": "7Aj2ozF5HRvdGi6utJRHJ3MRGG5ve77Ue1DFJr4VVQ7zVBzN4cKCmVPQqXUZ47sWm5Ldkd9FZCRLQPYnUu59Y2mTNvZAciD",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 70.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Coffee tastes much better when its payed for in crypto!",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:57.956819",
+            "modified_date": "2021-12-22 00:13:57.956837",
+            "author_name": "",
+            "author_email": "",
+            "id": "7BCaswPUhNA2",
+            "qr_img_url_xmr": "qrs/7BCaswPUhNA2.png",
+            "qr_img_url_btc": "qrs/tb1qn3w35av5.png",
+            "qr_img_url_bch": "qrs/qreh02e306xn.png",
+            "title": "Coffee",
+            "btc_address": "tb1qn3w35av5gl0w69palvhpzwgfkffh6ul0cesjt4",
+            "bch_address": "bchtest:qreh02e306xntuzvkdffc0j7g2uvrzr33c3wvwdc4d",
+            "xmr_address": "7BCaswPUhNA2uGY2qYHnqshMtuqNeQQgfVy4fA4jP9N5RR94i8ZLjviDDVmerFJCLU27n7Tk7w6avEqWykrAypjm2gwXmEw",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 14.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Make a miner happy!",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:58.063864",
+            "modified_date": "2021-12-22 00:13:58.063881",
+            "author_name": "",
+            "author_email": "",
+            "id": "78PtSMpAW7xE",
+            "qr_img_url_xmr": "qrs/78PtSMpAW7xE.png",
+            "qr_img_url_btc": "qrs/tb1qgrdrf90n.png",
+            "qr_img_url_bch": "qrs/qr90682483wz.png",
+            "title": "Send me 1 cent",
+            "btc_address": "tb1qgrdrf90nrgkgnhemhm6dtqskum984qpd5f4hwc",
+            "bch_address": "bchtest:qr90682483wzpzx8kj4nhmlvpa72vz0ecujuttu67q",
+            "xmr_address": "78PtSMpAW7xEWMB36dpjKPMmSjunTYBNFRaXxjahgpaGVs4mh2qSq9FPVy3jF5GnwyUXoL6AvX2JXD2uqkwwgDdh1tLvwN6",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 17276.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "Yep, another one.",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:58.166311",
+            "modified_date": "2021-12-22 00:13:58.166330",
+            "author_name": "",
+            "author_email": "",
+            "id": "78PtSMpAW7xE",
+            "qr_img_url_xmr": "qrs/78PtSMpAW7xE.png",
+            "qr_img_url_btc": "qrs/tb1qd6fzur35.png",
+            "qr_img_url_bch": "qrs/qrj37usvz80w.png",
+            "title": "Another one?",
+            "btc_address": "tb1qd6fzur359mmrew2k9t0ks5m7vnrhq5yvcetqgt",
+            "bch_address": "bchtest:qrj37usvz80wyjhr2vhgsnmpq2mhkwejeullwzf3xz",
+            "xmr_address": "78PtSMpAW7xEWMB36dpjKPMmSjunTYBNFRaXxjahgpaGVs4mh2qSq9FPVy3jF5GnwyUXoL6AvX2JXD2uqkwwgDdh1tLvwN6",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        },
+        {
+            "goal_usd": 172830.0,
+            "usd_total": 0,
+            "contributors": 0,
+            "description": "RQ? (:",
+            "percent": 0,
+            "hours": "",
+            "type": "gift",
+            "created_date": "2021-12-22 00:13:58.241307",
+            "modified_date": "2021-12-22 00:13:58.241327",
+            "author_name": "",
+            "author_email": "",
+            "id": "78PtSMpAW7xE",
+            "qr_img_url_xmr": "qrs/78PtSMpAW7xE.png",
+            "qr_img_url_btc": "qrs/tb1q07q6cvpn.png",
+            "qr_img_url_bch": "qrs/qphekq82ag2d.png",
+            "title": "Yet another one",
+            "btc_address": "tb1q07q6cvpna844dmn67r8hgnk7fx6dt20ry5mrx9",
+            "bch_address": "bchtest:qphekq82ag2duzz4tfr38dyp85qv02xumvcywclnvq",
+            "xmr_address": "78PtSMpAW7xEWMB36dpjKPMmSjunTYBNFRaXxjahgpaGVs4mh2qSq9FPVy3jF5GnwyUXoL6AvX2JXD2uqkwwgDdh1tLvwN6",
+            "btc_total": 0,
+            "xmr_total": 0,
+            "bch_total": 0,
+            "hour_goal": "",
+            "xmr_history": [],
+            "bch_history": [],
+            "btc_history": [],
+            "btc_confirmed": 0,
+            "btc_unconfirmed": 0,
+            "bch_confirmed": 0,
+            "bch_unconfirmed": 0
+        }
+    ],
+    "metadata": {
+        "total": 0,
+        "contributors": 0,
+        "modified": "2021-12-22 00:13:58.312025",
+        "title": "",
+        "description": "",
+        "image": "",
+        "url": "",
+        "viewkey": "0688faf6f342c5767208f874ff2159de7b8d0423d476573d286f6c4ae2d22e05",
+        "main_address": "5721UnYiXuMX73qGgL9nngPBX1t2cQT7pCw7gbW69ezZYB8Zo2GGE6tWUgnLcvdTWshBC4ik6Ty8SbtRWFvpbVBS5v9tSM1",
+        "protocol": "v3"
+    },
+    "archive": []
+}    
+    return wishlist
+}
+
+function getWishlist() {
+    let ran_int = Math.floor(Math.random() * 100000)
+    let url_get = 'http://109.228.52.142/funding/data/wishlist-data.json?uid=' + ran_int;
+    //let url_get = '../data/wishlist-data.json'
+    let something = {}
+    try {
+            $.ajax({
+            async: false,
+            type: 'GET',
+            url: url_get,
+            success: function(data) {
+                //something = data
+                wishlist = debugWishlist()
+                //wishlist = data
+                modified_live = wishlist["metadata"]["modified"]
+                //alert(modified_live)
+                if (modified != modified_live){
+                    modified = modified_live
+                    let html = '';
+                    wishlist["wishlist"].forEach(wish => {
+                        let percent_info = getTotal(wish)
+                        let sortme = percent_info["values"]
+                        console.log(sortme)
+                        let total = percent_info["total_usd"]
+                        
+                        sortme = getSortedKeys(sortme)
+                        console.log(sortme)
+
+                        //console.log(sorted)
+
+                        //total["usd"] = 
+                        //total["btc"] = %
+                        xmr_history = get_history(wish.xmr_history)
+                        bch_history = get_history(wish.bch_history)
+                        btc_history = get_history(wish.btc_history)
+                        wish.percent = total / wish.goal_usd * 100;
+
+                        //new progress bar
+                        //background: linear-gradient(to right, #ff4f00 -1000%, #0ac18e 40%, #f7931a 94%, red)
+                        colours = {
+                          "monero": "#f26822",
+                          "bitcoin-cash": "#0ac18e",
+                          "bitcoin": "#f7931a"
+                        }
+                        one = colours[sortme[0]] 
+                        prc0 = percent_info["values"][sortme[0]]
+                        one += ` ${prc0}%`
+
+
+                        two = colours[sortme[1]]
+                        prc1 = percent_info["values"][sortme[1]]
+                        prc1 += prc0
+                        two += ` ${prc0}% ${prc1}%`
+
+                        three = colours[sortme[2]]
+                        prc2 = percent_info["values"][sortme[2]]
+                        prc2 += prc1
+                        three += ` ${prc1}% ${prc2}%`
+
+                        end = `${prc2}%`
+                        let htmlSegment =`  
+                                            <style>
+                                             .progress_${wish["id"]} {
+                                             border-radius: 25px;
+                                             height:10px;
+                                             width:100%;
+                                             border:2px solid #000;
+                                             text-align:center;
+                                             color:#fff;
+                                             font-size:20px;
+                                             background: linear-gradient(to right,
+                                                 ${one}, ${two}, ${three}, transparent ${end});
+                                            }
+                                            </style>
+
+                                            <div class ="wish">
+                                            <span class="wish_title"><h3>${wish.title}</h3></span></br>
+                                            <div class="progress_${wish["id"]}"></div>
+                                                
+                                                <p class="fundgoal">Raised $${total} of $${wish.goal_usd} Contributors: ${wish.contributors}</p>
+                                                <p class="description">${wish.description}</p>
+
+                                                
+                                                <br/>
+<div class="tabs">
+  <input type="checkbox" name="tabs" id="tabone_${wish["id"]}">
+  <label class="cointab" for="tabone_${wish["id"]}"><img class="ticker" src="./logos/monero-xmr-logo.png" alt="XMR"></label>
+  <div class="tab">
+  <p class="xmr_address" id="${wish.id}">${wish.xmr_address}</br></p>
+  <p>[<a href="${wish.qr_img_url_xmr}" data-lightbox="${wish.id}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">${xmr_history}</span></span></p>
+  </div>
+  
+  <input type="checkbox" name="tabs" id="tabtwo_${wish["id"]}">
+  <label class="cointab" for="tabtwo_${wish["id"]}"><img class="ticker" src="./logos/bitcoin-cash-bch-logo.png" alt="BCH"></label>
+  <div class="tab">
+  <p class="bch_address" id="${wish.id}">${wish.bch_address}</br></p>
+  <p>[<a href="${wish.qr_img_url_bch}" data-lightbox="${wish.id}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">${bch_history}</span></span></p>
+  </div>
+  
+  <input type="checkbox" name="tabs" id="tabthree_${wish["id"]}">
+  <label class="cointab" for="tabthree_${wish["id"]}"><img class="ticker" src="./logos/BTC_Logo.png" alt="BTC"></label>
+  <div class="tab">
+  <p class="btc_address" id="${wish.id}">${wish.btc_address}</br></p> 
+  <p>[<a href="${wish.qr_img_url_btc}" data-lightbox="${wish.id}" data-title="Thank you 😘">QR</a>] <span class="tooltip">[History]<span class="tooltiptext">${btc_history}</span></span>
+  </div>
+</div>
+
+
+
+                                        </div> <hr>`;
+                        html += htmlSegment;
+                    });
+                    let container = document.querySelector('.wishlist');
+                    container.innerHTML = html;
+    }
+
+            }
+            });
+    } catch (error) {
+        return null;
+    }
+}
+//https://stackoverflow.com/questions/5199901/how-to-sort-an-associative-array-by-its-values-in-javascript/11811767
+function getSortedKeys(obj) {
+    var keys = Object.keys(obj);
+    return keys.sort(function(a,b){return obj[a] - obj[b]});
+}
+
+function getPrice(symbol){
+  let ran_int = Math.floor(Math.random() * 100000)
+  //alert(array_prices.length)
+  Object.keys(array_prices).forEach(function(symbol) {
+    let url_get = `https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd&uid=` + ran_int
+    try {
+         $.ajax({
+            async: false,
+            type: 'GET',
+            url: url_get,
+            success: function(data) {
+                //callback
+                array_prices[symbol] = data[symbol]["usd"]
+            }
+        });
+    }catch (error){
+        console.log(error)
+        return null;
+    }
+  });
+}
+
+
+function getTotal(wish){
+    let tickers = {
+        "monero": "xmr_total",
+        "bitcoin": "btc_total",
+        "bitcoin-cash": "bch_total"
+    }
+    let percentages = {
+      "values": {
+        "monero": 0,
+        "bitcoin-cash": 0,
+        "bitcoin": 0
+      },
+      "total_usd": 0
+    }
+
+    let total_usd = 0
+    let total_percent = 0
+    for (var symbol in tickers){
+        //array_prices = global
+        usd = array_prices[symbol]
+        per_coin = (usd * wish[tickers[symbol]])
+        percent = (per_coin / wish["goal_usd"]) * 100
+        total_percent += percent
+        percentages["values"][symbol] = percent
+        total_usd += per_coin
+     }
+     percentages["total_usd"] = total_usd.toFixed(2)
+     if (total_percent >= 100){
+      //set new values
+      percentages["values"].forEach(x => {
+        percentages["values"][x] = (percentages["values"][x] / total_percent) * 100
+      });
+
+     }
+     return percentages    
+}
+
+function get_history(inputs){
+    if (inputs.length){
+        //we have history
+        let i = 0
+        let history = ""
+        while(i < inputs.length){
+            history += ("+" + inputs.at(i).amount.toFixed(5) + "<br>")
+            if (i == 4){
+                i = inputs.length
+            }else{
+                i++
+            }
+        }
+        return history
+    }else{
+        return "No history😥"
+    }
+}
+
+function doit(){
+  getPrice()
+  getWishlist()
+}
+
+
+
+//on page load - render the wishlist. set a 'time updated variable from the json' then loop compare
+//infinite loop
+var array_prices = {"bitcoin-cash": 0, "monero": 0, "bitcoin": 0};
+function main(){
+    doit()
+  setInterval(doit,5000)
+}
+$(main)
+
